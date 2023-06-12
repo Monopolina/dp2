e0a8ae65f7871c00006c2351c8ae01228f80a4f0
polina <polinashatalova2003@mail.ru>
2023-06-13 02:20:37 +0800

    test yes

--- test/test.js
+++ test/test.js
@@ -1,7 +1,33 @@
-const assert = require('assert');
-const verifyUserAndPassword = require('../server/index');
+const mysql = require('mysql2');
+const { expect } = require('chai');
 
-it('server', () => {
-assert.equal((verifyUserAndPassword(  {body:{user: "user1", password: "1234"} }, { send: () => {''} }  )),
-(verifyUserAndPassword(  {body:{user: "user1", password: "1234"} }, { send: () => {''} }  )))
-})
+describe('MySQL Connection Pool', () => {
+  let pool;
+
+  before(() => {
+    pool = mysql.createPool({
+      connectionLimit: 5,
+      host: "localhost",
+      user: "pol",
+      database: "dp",
+      password: "1234",
+      dateStrings: true
+    });
+    global.pool = pool;
+  });
+
+  after(() => {
+    pool.end();
+  });
+
+  it('should acquire a connection from the pool', (done) => {
+    pool.getConnection((err, connection) => {
+      expect(err).to.be.null;
+      expect(connection).to.exist;
+      expect(connection).to.be.an('object');
+      expect(connection).to.have.property('release');
+      connection.release();
+      done();
+    });
+  });
+});
